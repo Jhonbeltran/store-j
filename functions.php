03ca5279c36844826d54a79467d702b95748ef6e
Jhonbeltran <jbeltranleon@gmail.com>
2017-09-27 21:18:48 -0500

    agrego un nuevo usuario usando el functions.php el cual puede realizar todo el CRUD en el sitio

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function add_role_viajero()
+{
+    remove_role( 'viajero' );
+    add_role(
+        'viajero',
+        'Viajero',
+        [
+            'read'         => true,
+            'edit_posts'   => true,
+            'upload_files' => true,
+            'publish_posts' => true,
+        ]
+    );
+}
+ 
+// add the simple_role
+add_action('init', 'add_role_viajero');
